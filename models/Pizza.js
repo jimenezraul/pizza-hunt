5ed8c026d56266c0f61b8918218176038fc9ae83
Raul <jimenezraul1981@gmail.com>
2022-04-13 01:16:07 -0400

    added validation

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,13 +1,17 @@
 const { Schema, model } = require("mongoose");
-const dateFormat = require('../utils/dateFormat');
+const dateFormat = require("../utils/dateFormat");
 
 const PizzaSchema = new Schema(
   {
     pizzaName: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdBy: {
       type: String,
+      required: true,
+      trim: true,
     },
     createdAt: {
       type: Date,
@@ -16,6 +20,8 @@ const PizzaSchema = new Schema(
     },
     size: {
       type: String,
+      required: true,
+      enum: ["Personal", "Small", "Medium", "Large", "Extra Large"],
       default: "Large",
     },
     toppings: [],
@@ -28,16 +34,19 @@ const PizzaSchema = new Schema(
   },
   {
     toJSON: {
-        virtuals: true,
-        getters: true
-      },
+      virtuals: true,
+      getters: true,
+    },
     id: false,
   }
 );
 
 // get total count of comments and replies on retrieval
-PizzaSchema.virtual('commentCount').get(function() {
-  return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
+  );
 });
 
 // create the Pizza model using the PizzaSchema
